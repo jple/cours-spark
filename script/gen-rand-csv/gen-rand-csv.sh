81f799a9efe507f8502d6a22dc2f8ca25d418196
Jean-Paul LE <jean-paul.le@edf.fr>
2019-01-01 23:49:53 +0100

    stream multi canal

--- script/gen-rand-csv/gen-rand-csv.sh
+++ script/gen-rand-csv/gen-rand-csv.sh
@@ -2,7 +2,7 @@ while true;
 do
 	# ecriture de csv dans la source1
 	filename=$(shuf -i 0-9999999 -n 1)
-	echo "id, val1, val2" >> dump/source1/$filename.csv
+	echo "id,val1,val2" >> dump/source1/$filename.csv
 	
 	for _ in `seq 1 5`
 	do
@@ -15,7 +15,7 @@ do
 
 	# ecriture de csv dans la source2
 	filename=$(shuf -i 0-9999999 -n 1)
-	echo "id, val1, val2" >> dump/source2/$filename.csv
+	echo "id,val1,val2" >> dump/source2/$filename.csv
 	
 	for _ in `seq 1 5`
 	do
--- script/gen-rand-csv/stream-csv.py
+++ script/gen-rand-csv/stream-csv.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
 
+import pyspark.sql.functions as F
+
 
 spark = SparkSession \
     .builder \
@@ -7,9 +9,30 @@ spark = SparkSession \
     .getOrCreate()
 
 
-s = spark.readStream.option("sep", ",").schema("id INT, val1 INT, val2 INT").csv('file:/root/dump/source1/', header=True)
-s.show()
-s.writeStream.format("console").option("truncate","false").start()
+# afficher les dataframe du stream
+s1 = spark.readStream.option("sep", ",").schema("id INT, val1 INT, val2 INT").csv('file:/root/dump/source1/', header=True)
+s1.writeStream.format("console").option("truncate","false").start()
+
+
+# combiner deux streams
+s1 = spark.readStream.option("sep", ",").schema("id INT, val1 INT, val2 INT").csv('file:/root/dump/source1/', header=True)
+s2 = spark.readStream.option("sep", ",").schema("id INT, val1 INT, val2 INT").csv('file:/root/dump/source2/', header=True)
+
+s1_wm = s1.withWatermark("s1_time", "3 seconds")
+s2_wm = s2.withWatermark("s2_time", "3 seconds")
+
+mean_val1_s1 = s1_wm.select(F.avg('val1')).withColumn('source', F.lit(1))
+mean_val1_s2 = s2_wm.select(F.avg('val1')).withColumn('source', F.lit(2))
+
+mean_val1 = mean_val1_s1.union(mean_val1_s2)
+mean_val1_s1.writeStream.format("console").option("truncate","false").start()
+
+
+
+
+
+mean_val1.writeStream.format("console").option("truncate","false").start()
 
 
 df = spark.read.option("delimiter", ",").option("header","true").csv('file:/root/dump/source1/', inferSchema=True)
+df.select(F.avg('val1')).withColumn('source', F.lit(1)).show()
