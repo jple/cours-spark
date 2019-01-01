6f6463ffae9050a107b487fb2edb1070196ba2ea
Jean-Paul LE <jean-paul.le@edf.fr>
2019-01-01 22:25:33 +0100

    corr syntax

--- script/gen-rand-csv/gen-rand-csv.sh
+++ script/gen-rand-csv/gen-rand-csv.sh
@@ -2,19 +2,28 @@ while true;
 do
 	# ecriture de csv dans la source1
 	filename=$(shuf -i 0-9999999 -n 1)
-	id=$(shuf -i 10000-99999 -n 1)
 	echo "id, val1, val2" >> dump/source1/$filename.csv
-	for numero in `seq 1 10`
-		echo $id, $(shuf -i 0-100 -n 1), $(shuf -i 0-500 -n 1) >> dump/source1/$filename.csv
+	
+	for _ in `seq 1 5`
+	do
+		idf=$(shuf -i 10000-99999 -n 1)
+		for _ in `seq 1 10`
+		do
+			echo $idf, $(shuf -i 0-100 -n 1), $(shuf -i 0-500 -n 1) >> dump/source1/$filename.csv
+		done
 	done
-
 
 	# ecriture de csv dans la source2
 	filename=$(shuf -i 0-9999999 -n 1)
-	id=$(shuf -i 10000-99999 -n 1)
 	echo "id, val1, val2" >> dump/source2/$filename.csv
-	for numero in `seq 1 10`
-		echo $id, $(shuf -i 0-100 -n 1), $(shuf -i 0-500 -n 1) >> dump/source2/$filename.csv
+	
+	for _ in `seq 1 5`
+	do
+		idf=$(shuf -i 10000-99999 -n 1)
+		for _ in `seq 1 10`
+		do
+			echo $idf, $(shuf -i 0-100 -n 1), $(shuf -i 0-500 -n 1) >> dump/source2/$filename.csv
+		done
 	done
 
 	sleep 1
