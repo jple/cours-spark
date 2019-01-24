71dc9f0c2c4daac16881c1907e59da66648067eb
jple <34452765+jple@users.noreply.github.com>
2019-01-24 22:12:21 +0100

    Update gen-rand-csv.sh

--- script/gen-rand-csv/gen-rand-csv.sh
+++ script/gen-rand-csv/gen-rand-csv.sh
@@ -2,29 +2,40 @@ while true;
 do
 	# ecriture de csv dans la source1
 	filename=$(shuf -i 0-9999999 -n 1)
-	echo "id,val1,val2" >> dump/source1/$filename.csv
+	echo "id,date,val1,val2" >> dump/gen-rand-csv/source1/$filename.csv
 	
 	for _ in `seq 1 5`
 	do
 		idf=$(shuf -i 10000-99999 -n 1)
-		for _ in `seq 1 10`
+		init_date=$(date -d '2018-02-03' '+%Y-%m-%d')
+		rand=$(shuf -i 0-3 -n 1)
+		rand_date=$(date -d "$init_date $rand days"  '+%Y-%m-%d')
+		for k in `seq 1 10`
 		do
-			echo $idf,$(shuf -i 0-100 -n 1),$(shuf -i 0-500 -n 1) >> dump/source1/$filename.csv
+			next_date=$(date -d "$rand_date $k days" "+%Y-%m-%d")
+			echo $idf,$next_date,$(shuf -i 0-100 -n 1),$(shuf -i 0-500 -n 1) >> dump/gen-rand-csv/source1/$filename.csv
 		done
 	done
 
+
 	# ecriture de csv dans la source2
 	filename=$(shuf -i 0-9999999 -n 1)
-	echo "id,val1,val2" >> dump/source2/$filename.csv
+	echo "id,val1,val2" >> dump/gen-rand-csv/source2/$filename.csv
 	
 	for _ in `seq 1 5`
 	do
-		idf=$(shuf -i 10000-99999 -n 1)
-		for _ in `seq 1 10`
+		idf=$(shuf -i 10000-99999 -n 1
+		init_date=$(date -d '2018-02-03' '+%Y-%m-%d')
+		rand=$(shuf -i 0-3 -n 1)
+		rand_date=$(date -d "$init_date $rand days"  '+%Y-%m-%d')
+		for k in `seq 1 10`
 		do
-			echo $idf,$(shuf -i 0-100 -n 1),$(shuf -i 0-500 -n 1) >> dump/source2/$filename.csv
+			next_date=$(date -d "$rand_date $k days" "+%Y-%m-%d")
+			echo $idf,$next_date,$(shuf -i 0-100 -n 1),$(shuf -i 0-500 -n 1) >> dump/gen-rand-csv/source2/$filename.csv
 		done
 	done
+
+
 
 	sleep 1
 done
